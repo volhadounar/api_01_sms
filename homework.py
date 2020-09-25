4f8b5164814e1de470f694955c0571d736b614b8
Olga Dovnar <olga.dovnar@live.com>
2020-09-25 23:43:00 +0300

    my 1st

--- homework.py
+++ homework.py
@@ -1,20 +1,39 @@
+
+import os
 import time
 
+from dotenv import load_dotenv
+
 import requests
-from twilio.rest import Client
+
+import twilio.rest
+
+load_dotenv()
+
+account_sid = os.getenv('account_sid')
+auth_token = os.getenv('auth_token')
+client = twilio.rest.Client(account_sid, auth_token)
 
 
 def get_status(user_id):
     params = {
-        ...
+        'user_id': 11993562,
+        'fields': 'online',
+        'access_token': os.getenv('vk_access_token'),
+        'v': 5.92,
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    response = requests.post('https://api.vk.com/method/users.get/',
+                             params=params)
+    return response.json()['response'][0]['online']
 
 
-def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+def sms_sender(smste_xt):
+    message = client.messages.create(
+                body=smste_xt,
+                from_=os.getenv('NUMBER_FROM'),
+                to=os.getenv('NUMBER_TO')
+            )
+    return message.sid
 
 
 if __name__ == "__main__":
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,11 @@ from os.path import dirname
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
 
-os.environ['ACCOUNT_SID'] = ''
-os.environ['AUTH_TOKEN'] = ''
-os.environ['VK_TOKEN'] = ''
-os.environ['NUMBER_FROM'] = ''
-os.environ['NUMBER_TO'] = ''
+os.environ['ACCOUNT_SID'] = 'AC518aee95826d5273e616ce07d22a5f09'
+os.environ['AUTH_TOKEN'] = '2e5cd9d943fe45378fee4f2f0b359b5b'
+os.environ['VK_TOKEN'] = 'a754052d2193fc867dd8f5b7f2b6d73740d92912d5d1ce1f2ea4e41125cdc40dccefb01a859ad09632cd7'
+os.environ['NUMBER_FROM'] = '+12185173754'
+os.environ['NUMBER_TO'] = '+18564145634'
 
 pytest_plugins = [
     'tests.fixtures.fixture_twilio',
